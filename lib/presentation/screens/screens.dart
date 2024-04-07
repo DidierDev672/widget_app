9063308e96b00a5623a0c75ea14cc95acbc8a94c
DidierDev672 <ariasdev672@gmail.com>
2024-04-06 23:28:17 -0500

    screens creadas y barril actualizado

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,8 @@
 export 'buttons/button_screens.dart';
 export 'cards/cards_screen.dart';
 export 'home/home_screen.dart';
+export 'animated/animated_screen.dart';
+export 'infinite_scroll/infinite_scroll_screen.dart';
+export 'progress/progress_screen.dart';
+export 'snackbar/snackbar_screen.dart';
+export 'ui_controls/ui_controls_screen.dart';
