0036aac943cd71c2d01378659e75cc5999d6da40
DidierDev672 <ariasdev672@gmail.com>
2024-04-06 23:45:26 -0500

    Correcion de la elavation de las cards

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 const cards = <Map<String, dynamic>>[
-  {'elevation': '0.0', 'label': 'Elevation 0'},
-  {'elevation': '1.0', 'label': 'Elevation 1'},
-  {'elevation': '2.0', 'label': 'Elevation 2'},
-  {'elevation': '3.0', 'label': 'Elevation 3'},
-  {'elevation': '4.0', 'label': 'Elevation 4'},
-  {'elevation': '5.0', 'label': 'Elevation 5'},
+  {'elevation': 0.0, 'label': 'Elevation 0'},
+  {'elevation': 1.0, 'label': 'Elevation 1'},
+  {'elevation': 2.0, 'label': 'Elevation 2'},
+  {'elevation': 3.0, 'label': 'Elevation 3'},
+  {'elevation': 4.0, 'label': 'Elevation 4'},
+  {'elevation': 5.0, 'label': 'Elevation 5'},
 ];
 
 class CardsScreens extends StatelessWidget {
